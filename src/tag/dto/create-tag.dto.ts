2962f5d7d94c05821593dac22cafb8a2ee6d2905
Alexander Smith <hongzeyuan0@gmail.com>
2025-10-20 22:04:55 +0800

    feat: 添加验证规则到 CreateTagDto 和 UpdateTagDto

--- src/tag/dto/create-tag.dto.ts
+++ src/tag/dto/create-tag.dto.ts
@@ -1,1 +1,9 @@
-export class CreateTagDto {}
+import { IsNotEmpty } from 'class-validator';
+
+export class CreateTagDto {
+  @IsNotEmpty({ message: '标签名称不能为空' })
+  name: string;
+
+  @IsNotEmpty({ message: '标签图标不能为空' })
+  icon: string;
+}
--- src/tag/dto/update-tag.dto.ts
+++ src/tag/dto/update-tag.dto.ts
@@ -1,4 +1,8 @@
+import { IsNotEmpty } from 'class-validator';
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateTagDto } from './create-tag.dto';
 
-export class UpdateTagDto extends PartialType(CreateTagDto) {}
+export class UpdateTagDto extends PartialType(CreateTagDto) {
+  @IsNotEmpty({ message: '标签ID不能为空' })
+  id: string;
+}
