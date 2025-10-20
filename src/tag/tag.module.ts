717163b963b8436ed95b61c2931d905506281d88
Alexander Smith <hongzeyuan0@gmail.com>
2025-10-20 22:06:16 +0800

    feat: 在 TagModule 中添加 TypeOrmModule 以支持 Tag 实体

--- src/tag/tag.module.ts
+++ src/tag/tag.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { TagService } from './tag.service';
 import { TagController } from './tag.controller';
+import { Tag } from './entities/tag.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Tag])],
   controllers: [TagController],
   providers: [TagService],
 })
