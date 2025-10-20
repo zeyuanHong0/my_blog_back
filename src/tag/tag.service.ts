79df3d8391e0fe859dbde1ce3841a0643e90530d
Alexander Smith <hongzeyuan0@gmail.com>
2025-10-20 22:06:30 +0800

    feat: 更新 TagService 以使用 TypeORM 存储库进行数据操作，并修复 Tag 实体导入路径

--- src/tag/tag.service.ts
+++ src/tag/tag.service.ts
@@ -1,26 +1,66 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+
 import { CreateTagDto } from './dto/create-tag.dto';
 import { UpdateTagDto } from './dto/update-tag.dto';
+import { Tag } from './entities/tag.entity';
 
 @Injectable()
 export class TagService {
-  create(createTagDto: CreateTagDto) {
-    return 'This action adds a new tag';
+  constructor(
+    @InjectRepository(Tag)
+    private readonly tagRepository: Repository<Tag>,
+  ) {}
+
+  async create(createTagDto: CreateTagDto) {
+    await this.tagRepository.save(createTagDto);
+    return {
+      code: 200,
+      msg: '操作成功',
+    };
   }
 
-  findAll() {
-    return `This action returns all tag`;
+  async findAll() {
+    const tagList = await this.tagRepository.find({
+      where: {
+        is_delete: 0,
+      },
+      select: ['id', 'name'],
+    });
+    return {
+      code: 200,
+      msg: '操作成功',
+      data: tagList,
+    };
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} tag`;
+  async findOne(id: string) {
+    const tag = await this.tagRepository.findOne({
+      where: { id, is_delete: 0 },
+      select: ['id', 'name', 'icon', 'createTime', 'updateTime'],
+    });
+    return {
+      code: 200,
+      msg: '操作成功',
+      data: tag,
+    };
   }
 
-  update(id: number, updateTagDto: UpdateTagDto) {
-    return `This action updates a #${id} tag`;
+  async update(updateTagDto: UpdateTagDto) {
+    const { id, ...rest } = updateTagDto;
+    await this.tagRepository.update(id, rest);
+    return {
+      code: 200,
+      msg: '操作成功',
+    };
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} tag`;
+  async remove(id: string) {
+    await this.tagRepository.update(id, { is_delete: 1 });
+    return {
+      code: 200,
+      msg: '操作成功',
+    };
   }
 }
