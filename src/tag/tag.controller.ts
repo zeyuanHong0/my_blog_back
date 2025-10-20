6434924d1a25e67238a3dcecb05d24c175ec0325
Alexander Smith <hongzeyuan0@gmail.com>
2025-10-20 22:06:10 +0800

    feat: 更新 TagController 的路由和方法，添加 JWT 认证

--- src/tag/tag.controller.ts
+++ src/tag/tag.controller.ts
@@ -3,40 +3,42 @@ import {
   Get,
   Post,
   Body,
-  Patch,
   Param,
   Delete,
+  UseGuards,
 } from '@nestjs/common';
+import { JwtAuthGuard } from '@/auth/guards/jwt-auth.guard';
 import { TagService } from './tag.service';
 import { CreateTagDto } from './dto/create-tag.dto';
 import { UpdateTagDto } from './dto/update-tag.dto';
 
+@UseGuards(JwtAuthGuard)
 @Controller('tag')
 export class TagController {
   constructor(private readonly tagService: TagService) {}
 
-  @Post()
+  @Post('createTag')
   create(@Body() createTagDto: CreateTagDto) {
     return this.tagService.create(createTagDto);
   }
 
-  @Get()
+  @Get('getAllTagList')
   findAll() {
     return this.tagService.findAll();
   }
 
-  @Get(':id')
+  @Get('getTagInfo/:id')
   findOne(@Param('id') id: string) {
-    return this.tagService.findOne(+id);
+    return this.tagService.findOne(id);
   }
 
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateTagDto: UpdateTagDto) {
-    return this.tagService.update(+id, updateTagDto);
+  @Post('updateTag')
+  update(@Body() updateTagDto: UpdateTagDto) {
+    return this.tagService.update(updateTagDto);
   }
 
-  @Delete(':id')
+  @Delete('deleteTag/:id')
   remove(@Param('id') id: string) {
-    return this.tagService.remove(+id);
+    return this.tagService.remove(id);
   }
 }
