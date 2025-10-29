9cbdb933ff54809e4a0b38c26855eef9b8aff1b3
Alexander Smith <hongzeyuan0@gmail.com>
2025-10-29 21:20:51 +0800

    feat: 更新 BlogService 以支持用户 ID 传递和标签关联验证

--- src/blog/blog.service.ts
+++ src/blog/blog.service.ts
@@ -4,8 +4,8 @@ import { Repository, In } from 'typeorm';
 import { CreateBlogDto } from './dto/create-blog.dto';
 import { UpdateBlogDto } from './dto/update-blog.dto';
 import { Blog } from './entities/blog.entity';
-import { Tag } from '../tag/entities/tag.entity';
-import { User } from '../user/entities/user.entity';
+import { Tag } from '@/tag/entities/tag.entity';
+import { User } from '@/user/entities/user.entity';
 
 @Injectable()
 export class BlogService {
@@ -18,12 +18,12 @@ export class BlogService {
     private readonly userRepository: Repository<User>,
   ) {}
 
-  async create(createBlogDto: CreateBlogDto): Promise<Blog> {
-    const { tagIds, createUser, ...blogData } = createBlogDto;
+  async create(createBlogDto: CreateBlogDto, userId: string) {
+    const { tags, ...blogData } = createBlogDto;
 
     // 验证用户是否存在
     const user = await this.userRepository.findOne({
-      where: { id: createUser },
+      where: { id: userId },
     });
     if (!user) {
       throw new NotFoundException('用户不存在');
@@ -35,25 +35,31 @@ export class BlogService {
       createUser: user,
     });
 
-    // 如果有标签 ID，查找并关联标签
-    if (tagIds && tagIds.length > 0) {
-      const tags = await this.tagRepository.find({
-        where: { id: In(tagIds) },
+    // 如果有标签，查找并关联标签
+    if (tags && tags.length > 0) {
+      const findTags = await this.tagRepository.find({
+        where: { id: In(tags) },
       });
-      blog.tags = tags;
+      if (findTags.length !== tags.length) {
+        throw new NotFoundException('部分标签不存在');
+      }
+      blog.tags = findTags;
     }
-
-    return await this.blogRepository.save(blog);
+    await this.blogRepository.save(blog);
+    return {
+      code: 200,
+      message: '博客创建成功',
+    };
   }
 
-  async findAll(): Promise<Blog[]> {
+  async findAll() {
     return await this.blogRepository.find({
       relations: ['createUser', 'tags'],
       where: { is_delete: 0 },
     });
   }
 
-  async findOne(id: string): Promise<Blog> {
+  async findOne(id: string) {
     const blog = await this.blogRepository.findOne({
       where: { id, is_delete: 0 },
       relations: ['createUser', 'tags'],
@@ -64,7 +70,7 @@ export class BlogService {
     return blog;
   }
 
-  async update(id: string, updateBlogDto: UpdateBlogDto): Promise<Blog> {
+  async update(id: string, updateBlogDto: UpdateBlogDto) {
     const { tagIds, ...blogData } = updateBlogDto;
 
     const blog = await this.findOne(id);
@@ -87,7 +93,7 @@ export class BlogService {
     return await this.blogRepository.save(blog);
   }
 
-  async remove(id: string): Promise<void> {
+  async remove(id: string) {
     const blog = await this.findOne(id);
     blog.is_delete = 1;
     await this.blogRepository.save(blog);
