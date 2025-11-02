dae24e8268b2cb1c2aa4dd5b43affe53ac29618e
Alexander Smith <hongzeyuan0@gmail.com>
2025-11-02 21:18:57 +0800

    feat: 更新UpdateBlogDto以要求包含id并移除tagIds字段

--- src/blog/dto/update-blog.dto.ts
+++ src/blog/dto/update-blog.dto.ts
@@ -1,10 +1,8 @@
 import { PartialType } from '@nestjs/mapped-types';
-import { IsOptional, IsArray, IsUUID } from 'class-validator';
+import { IsNotEmpty } from 'class-validator';
 import { CreateBlogDto } from './create-blog.dto';
 
 export class UpdateBlogDto extends PartialType(CreateBlogDto) {
-  @IsArray()
-  @IsOptional()
-  @IsUUID('4', { each: true })
-  tagIds?: string[];
+  @IsNotEmpty()
+  id: string;
 }
