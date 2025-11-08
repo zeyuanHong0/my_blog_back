8902a2171ff1a263d46951d4d2626f217a274b3b
Alexander Smith <hongzeyuan0@gmail.com>
2025-11-08 20:07:12 +0800

    feat: 添加 BlogFrontController 到 BlogModule 以支持前端博客接口

--- src/blog/blog.module.ts
+++ src/blog/blog.module.ts
@@ -2,13 +2,14 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { BlogService } from './blog.service';
 import { BlogController } from './blog.controller';
+import { BlogFrontController } from './blog.front.controller';
 import { Blog } from './entities/blog.entity';
-import { Tag } from '../tag/entities/tag.entity';
-import { User } from '../user/entities/user.entity';
+import { Tag } from '@/tag/entities/tag.entity';
+import { User } from '@/user/entities/user.entity';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Blog, Tag, User])],
-  controllers: [BlogController],
+  controllers: [BlogController, BlogFrontController],
   providers: [BlogService],
 })
 export class BlogModule {}
