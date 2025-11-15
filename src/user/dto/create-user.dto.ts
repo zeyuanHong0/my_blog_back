b6dce7706a68fdff530c1a1fe538e570af7321a5
Alexander Smith <hongzeyuan0@gmail.com>
2025-11-15 14:59:42 +0800

    feat: 更新 CreateUserDto 以使用用户名并添加邮箱格式验证

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,13 +1,14 @@
-import { IsNotEmpty, MinLength } from 'class-validator';
+import { IsNotEmpty, MinLength, IsEmail } from 'class-validator';
 
 export class CreateUserDto {
   @IsNotEmpty()
-  name: string;
+  username: string;
 
   @IsNotEmpty()
   @MinLength(6)
   password: string;
 
   @IsNotEmpty()
+  @IsEmail({}, { message: '邮箱格式不正确' })
   email: string;
 }
