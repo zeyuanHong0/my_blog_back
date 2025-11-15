c9128b1cc99e64fce9eefd1a6d07e8972e22d435
Alexander Smith <hongzeyuan0@gmail.com>
2025-11-15 15:00:18 +0800

    feat: 添加文件上传模块，包括控制器、服务和实体

---
+++ src/upload/upload.controller.ts
@@ -0,0 +1,51 @@
+import {
+  Controller,
+  Post,
+  Req,
+  UploadedFile,
+  UseInterceptors,
+} from '@nestjs/common';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { diskStorage } from 'multer';
+import { extname, join } from 'path';
+import type { Request } from 'express';
+
+import { UploadService } from './upload.service';
+
+@Controller('upload')
+export class UploadController {
+  constructor(private readonly uploadService: UploadService) {}
+
+  @Post('file')
+  @UseInterceptors(
+    FileInterceptor('file', {
+      storage: diskStorage({
+        destination: join(process.cwd(), 'files'), // 存储目录
+        filename: (req, file, callback) => {
+          const uniqueSuffix =
+            Date.now() + '-' + Math.round(Math.random() * 1e9);
+          const ext = extname(file.originalname);
+          callback(null, `${uniqueSuffix}${ext}`);
+        },
+      }),
+      limits: { fileSize: 5 * 1024 * 1024 }, // 限制 5MB
+    }),
+  )
+  async uploadFile(
+    @UploadedFile() file: Express.Multer.File,
+    @Req() req: Request,
+  ) {
+    console.log('uploadFile', file);
+    const fileRecord = await this.uploadService.saveFileRecord(file);
+    const host = `${req.protocol}://${req.get('host')}`;
+    const data = {
+      fileUrl: `${host}/files/${file.filename}`,
+      ...fileRecord,
+    };
+    return {
+      code: 200,
+      message: '上传成功',
+      data,
+    };
+  }
+}
