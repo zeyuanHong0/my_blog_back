4b28b0a81ad91c1283da93ef8b33a2a4ecf81809
Alexander Smith <hongzeyuan0@gmail.com>
2025-11-15 14:59:33 +0800

    fix: 更新导入路径以使用绝对路径引用 UserModule

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,10 +3,10 @@ import { PassportModule } from '@nestjs/passport';
 import { JwtModule } from '@nestjs/jwt';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 
-import { ConfigEnum } from '../enum/config.enum';
+import { ConfigEnum } from '@/enum/config.enum';
+import { UserModule } from '@/user/user.module';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
-import { UserModule } from '../../src/user/user.module';
 import { LocalStrategy } from './local.strategy';
 import { JwtStrategy } from './jwt.strategy';
 
