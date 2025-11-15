183c2dc4f32ad4c59c21170b11aa94d1a6e4a3c2
Alexander Smith <hongzeyuan0@gmail.com>
2025-11-15 22:06:10 +0800

    feat: 添加响应拦截器以统一 API 响应格式

---
+++ src/common/interceptors/response.interceptor.ts
@@ -0,0 +1,39 @@
+import {
+  Injectable,
+  NestInterceptor,
+  ExecutionContext,
+  CallHandler,
+} from '@nestjs/common';
+import { map } from 'rxjs/operators';
+import { Observable } from 'rxjs';
+import { Request } from 'express';
+
+@Injectable()
+export class ResponseInterceptor implements NestInterceptor {
+  intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
+    const request: Request = context.switchToHttp().getRequest();
+    const method = request.method;
+
+    const defaultMsgMap = {
+      GET: '获取成功',
+      POST: '创建成功',
+      PUT: '更新成功',
+      DELETE: '删除成功',
+    };
+
+    return next.handle().pipe(
+      map((responseData) => {
+        const { data, msg, code } =
+          responseData && typeof responseData === 'object'
+            ? responseData
+            : { data: responseData };
+
+        return {
+          code: code ?? 200,
+          msg: msg ?? defaultMsgMap[method] ?? '操作成功',
+          data,
+        };
+      }),
+    );
+  }
+}
