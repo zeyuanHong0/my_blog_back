3e9bce59d4483e81160c2d64ff44ec6e72f38f7d
Alexander Smith <hongzeyuan0@gmail.com>
2025-11-15 22:06:10 +0800

    feat: 添加响应拦截器以统一 API 响应格式

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { Logger } from 'nestjs-pino';
 
 import { AppModule } from './app.module';
 import { AllExceptionsFilter } from './filters/all-exceptions.filter';
+import { ResponseInterceptor } from '@/common/interceptors/response.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
@@ -12,6 +13,7 @@ async function bootstrap() {
   app.useLogger(app.get(Logger)); //使用nestjs-pino作为日志记录器
   app.setGlobalPrefix('api'); //设置全局前缀为 'api'
   app.useGlobalFilters(new AllExceptionsFilter(app.get(Logger)));
+  app.useGlobalInterceptors(new ResponseInterceptor());
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true, // 自动去除非 DTO 定义的属性
